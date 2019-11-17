f58b61c268d340417e3c592be93b9304a7efe825
Hjk <adm-vp@Hjks-MBP.lan>
2019-11-17 21:47:56 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from utils import *
+
+vocabulary = load_vocabulary()
+
+
+
+inverted_index = load_inverted_index_1()
+
+inverted_index_2 = load_inverted_index_2()
+doc_norm = load_doc_norm()
+        
+name_index = load_from_json('name_index.json')        
+name_norm = load_from_json('name_norm.json')
+
+
